ea82dfbf7351a5e2b6648297977425d0d2be5902
Kumachinomi <nomizu7412@gmail.com>
2024-12-05 10:12:35 +0900

    DjangoのAPIを叩く実装

---
+++ visualization-app-next/frontend/pages/api/mermaid-diagrams.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const handler = async(req, res) => {
+    if(!['GET','POST'].includes(req.method)){
+        return res.status(405).end();
+    }
+
+    const DJANGO_API_URL = 'http://localhost:8000/api/mermaid-diagrams/';
+
+    try{
+        if (req.method === 'POST'){
+            const response = await axios.post(DJANGO_API_URL, req.body);
+            return res.status(200).json(response.data);
+        }
+
+        if(req.method === 'GET'){
+            const response = await axios.get(DJANGO_API_URL);
+            return res.status(200).json(response.data);
+        }
+        } catch (error) {
+            return res.status(500).json({ error: 'Server error' });
+        }
+    };
+
+export default handler;
+
+
+
