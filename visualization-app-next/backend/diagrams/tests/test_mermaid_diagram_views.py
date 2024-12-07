ee4c8221463ad5fa1dc4876d30940be7bb3fcaad
Kumachinomi <nomizu7412@gmail.com>
2024-12-08 07:32:09 +0900

    タイトルが空の場合に保存が失敗することのテスト追加とテストコメントの追加

--- visualization-app-next/backend/diagrams/tests/test_mermaid_diagram_views.py
+++ visualization-app-next/backend/diagrams/tests/test_mermaid_diagram_views.py
@@ -18,6 +18,7 @@ def mermaid_diagram():
 @pytest.mark.django_db
 class TestMermaidDiagramViewSet:
     def test_list_diagrams(self, api_client, mermaid_diagram):
+        """一覧取得ができることのテスト"""
         url = reverse('mermaiddiagram-list')
         response = api_client.get(url)
         
@@ -27,6 +28,7 @@ class TestMermaidDiagramViewSet:
         assert response.data[0]['diagram_data'] == mermaid_diagram.diagram_data
 
     def test_create_diagram(self, api_client):
+        """保存ができることのテスト"""
         url = reverse('mermaiddiagram-list')
         request_data = {
             'title': 'New Diagram',
@@ -38,6 +40,19 @@ class TestMermaidDiagramViewSet:
         assert MermaidDiagram.objects.count() == 1
         assert response.data['title'] == request_data['title']
         assert response.data['diagram_data'] == request_data['diagram_data']
+    
+    def test_create_diagram_invalid_data(self,api_client):
+        """タイトルが空の場合、保存が失敗することをテスト"""
+        url = reverse('mermaiddiagram-list')
+        data = {
+            'title': '',
+            'diagram_data': 'graph LR;\nA-->B;'
+        }
+        response = api_client.post(url, data, format='json')
+
+        assert response.status_code == 400
+        assert 'title' in response.data # タイトルに関するエラー情報があることを確認
+
 
 
 
