e5fe54a3fdc0b8d579663d254c424c86d0031165
Kumachinomi <nomizu7412@gmail.com>
2024-12-10 16:03:01 +0900

    命名の修正

--- visualization-app-next/frontend/next.config.mjs
+++ visualization-app-next/frontend/next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone' 
+    output: 'standalone',
+    webpack: (config) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+      return config;
+    },
+    transpilePackages: ['mermaid'],
+    experimental: {
+      appDir: true
+    }
   };
   
   export default nextConfig;
