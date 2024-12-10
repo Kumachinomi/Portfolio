ccb9a791e3eb5c7bbd3badc4cb08d737a39d0e8e
Kumachinomi <nomizu7412@gmail.com>
2024-12-10 16:07:18 +0900

    export構文のエラー修正

--- visualization-app-next/frontend/next.config.js
+++ visualization-app-next/frontend/next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'standalone' 
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
+  transpilePackages: ['mermaid'],
+  experimental: {
+    appDir: true
+  }
+};
+
+module.exports = nextConfig;
